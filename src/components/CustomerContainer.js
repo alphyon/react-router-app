45b0c9cd3d4ef392d1ae0323cc90187b7d1cddb3
José Antonio Henríquez Chavarría <alphyon21@gmail.com>
2018-08-19 06:09:58 -0600

    agregados uso de control dinamico

--- src/components/CustomerContainer.js
+++ src/components/CustomerContainer.js
@@ -4,12 +4,17 @@ import PropTypes from 'prop-types';
 import AppFrame from './AppFrame';
 import { getCustomerByDui } from '../selector/customers';
 import { Route } from 'react-router-dom';
+import CustomerEdit from './CustomerEdit';
+import CustomerData from './CustomerData';
 
 class CustomerContainer extends Component {
     renderBody=()=>(
         <Route path="/customers/:dui/edit" children={
-            ({match})=>(match ? <p>Edicion </p>: <p>No Edit</p>)
-        }/>
+            ({match})=>{
+               const ControlDinamic = match ? CustomerEdit : CustomerData;
+               return <ControlDinamic {...this.props.customer} />
+            }
+        } />
     )
     render() {
         //<p>Datos de del cliente { this.props.customer.name}</p>

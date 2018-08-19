9c98a18cc33b861fb1f5e379a971c92495c44214
José Antonio Henríquez Chavarría <alphyon21@gmail.com>
2018-08-18 21:49:58 -0600

    add redux promise

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,13 +2,6 @@ import { FETCH_CUSTOMER } from '../constants'
 import { createAction } from 'redux-actions';
 
 
-const customers = [
-    { "dui": "010234", "name": "Juan perez", "age": 30 },
-    { "dui": "010235", "name": "Juan perez", "age": 30 },
-    { "dui": "010236", "name": "Juan perez", "age": 30 },
-    { "dui": "010237", "name": "Juan perez", "age": 30 },
-    { "dui": "010238", "name": "Juan perez", "age": 30 },
-    { "dui": "010239", "name": "Juan perez", "age": 30 },
-];
-
-export const fetchCustomers = createAction(FETCH_CUSTOMER, ()=> customers)
+const url = "http://localhost:3001/customers";
+const apiFetch = () => fetch(url).then(v=> v.json());
+export const fetchCustomers = createAction(FETCH_CUSTOMER, apiFetch)
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,9 @@
-import {createStore, compose} from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import promiseMiddleware from 'redux-promise';
 import reducers from '../reducers';
 
-const composeEnshancer = window.__REDUX_DEVTOOLS_EXTENSION__ || compose ; //par lanzar plugin en chrome para depurar 
+const composeEnshancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; //par lanzar plugin en chrome para depurar 
 //params 1-reductores, 2 -estado que devuelve
 
 
-export const store = createStore(reducers, {},composeEnshancer())
+export const store = createStore(reducers,composeEnshancer(applyMiddleware(promiseMiddleware)))
